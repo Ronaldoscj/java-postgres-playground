175deb66dedb019d6b7c5e0952bc58aeeddae412
Ronaldoscj siqueira <ronaldoscj@hotmail.com>
2023-08-17 22:59:58 +0000

    criação vetor

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -1,7 +1,23 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class AppArrays {
     public static void main(String[] args) {
+        double vetor[] = {10, 20, 30};
+        System.out.println(Arrays.toString(vetor));
+
+        System.out.println("valor da posição 0: " + vetor[0]);
+        System.out.println("valor da posição 1: " + vetor[1]);
+        System.out.println("valor da posição 2: " + vetor[2]);
+        System.out.println("valor da posição 3: " + vetor[3]);
+
+
+
+
+
+
+
         
     }
 }

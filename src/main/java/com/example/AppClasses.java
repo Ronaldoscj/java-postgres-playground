c8815aadd16d31938c76d82b629f4100c9700727
Ronaldoscj siqueira <ronaldoscj@hotmail.com>
2023-08-18 00:39:56 +0000

    run

---
+++ src/main/java/com/example/AppClasses.java
@@ -0,0 +1,13 @@
+package com.example;
+
+class Cliente{
+    private double renda;
+    private char sexo;
+    private int anoNascimento;
+}
+
+public class AppClasses {
+    public static void main(String[] args) {
+        
+    }
+}
